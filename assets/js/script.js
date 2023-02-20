caf9aa609520691ce548950c28a8beb9ab919c41
sydnief <sydniefarrell@gmail.com>
2023-02-20 00:22:11 -0500

    started timer function/ cleaned code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,32 @@
-// TIMER: STARTS COUNTDOWN FROM 5 MINUTES ON CLICK START BUTTON, DECREASE 5 SECONDS EVERY WRONG ANSWER
+var buttonClick = document.querySelector(".button"); // Select Button
+var timerSet = document.querySelector("#timer"); // Select Timer
 
-// Function for creating a countdown timer
+// Timer that counts down from 30 once start button is clicked 
+// (add code that decreases 5 seconds every time wrong answer is selected)
+function timeRemaining() {
+    var timeLeft = 30; // Set starting time
 
-// Event listener to start button to begin timer/first question
-var timerStart = document.querySelector(".button");
-var timer = document.querySelector("#timer");
-// timerStart.addEventListener("click", function())
+    var timerInterval = setInterval(function() {
+
+        if (timeLeft > 1) {
+            timerSet.textContent = timeLeft + ' seconds remaining';
+            timeLeft--
+         } else if(timeLeft === 1) {
+            timerSet.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+         } else {
+            timerSet.textContent = '';
+            clearInterval(timerInterval)
+            // Create a function to display message that time has run out below this function
+            // sendMessage();
+         }
+    }, 1000);
+}
+
+// Event Listener to click button to start timer
+buttonClick.addEventListener("click", timeRemaining());
+
+
 // QUESTIONS
 
 // Function for creating first question form
