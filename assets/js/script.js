99143006c6508d8fddf28e46a2f30c1cf2243006
sydnief <sydniefarrell@gmail.com>
2023-02-19 20:44:54 -0500

    added comments to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// TIMER: STARTS COUNTDOWN FROM 5 MINUTES ON CLICK START BUTTON, DECREASE 5 SECONDS EVERY WRONG ANSWER
+
+// Function for creating a countdown timer
+
+// Event listener to start button to begin timer/first question
+
+// QUESTIONS
+
+// Function for creating first question form
+
+// Event listener to submit first question/begin second question
+
+// Function for creating second question form
+
+// Event listener to submit second question/begin third question
+
+// Function for creating third question form
+
+// Event listener to 
+
+// GAME OVER: SAVE SCORE AND 
